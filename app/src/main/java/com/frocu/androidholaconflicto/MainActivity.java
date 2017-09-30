652a09ee0661cbc4f02fb12a8f300eafab7259a6
Programador 2 <programadordos@ejemplo.org>
2017-09-30 11:49:01 +0200

    Programador 2 pone un comentario en MainActivity.java

--- app/src/main/java/com/frocu/androidholaconflicto/MainActivity.java
+++ app/src/main/java/com/frocu/androidholaconflicto/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //comentario añadido por el programador 2
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
